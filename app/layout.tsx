2209a2dce75ed94b09c46b6426947e6c21aaf6de
JaeHxn <wkdwogns2345@naver.com>
2026-03-03 22:24:05 +0900

    fix: add meta tag and robots.txt for Adsense verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,6 +30,7 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <head>
+        <meta name="google-adsense-account" content="ca-pub-5354319294441406" />
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5354319294441406"
